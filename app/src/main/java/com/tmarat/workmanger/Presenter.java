c79106cd964643dee7a866eebf8f9cc5af634296
Marat Tlepberghenov <mur.marat@gmail.com>
2018-06-23 13:08:06 +0300

    Creates constructor

--- app/src/main/java/com/tmarat/workmanger/Presenter.java
+++ app/src/main/java/com/tmarat/workmanger/Presenter.java
@@ -2,4 +2,11 @@ package com.tmarat.workmanger;
 
 public class Presenter implements Contract.Presenter {
 
+  private Contract.View view;
+  private Contract.Model model;
+
+  public Presenter(Contract.View view) {
+    this.view = view;
+    model = new Model();
+  }
 }
