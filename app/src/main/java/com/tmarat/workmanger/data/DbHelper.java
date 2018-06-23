be1e9f8e3dd727d0770ba0d851da06c7287e050a
Marat Tlepberghenov <mur.marat@gmail.com>
2018-06-23 14:41:20 +0300

    Creates db helper class for work with data base

---
+++ app/src/main/java/com/tmarat/workmanger/data/DbHelper.java
@@ -0,0 +1,21 @@
+package com.tmarat.workmanger.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+  public DbHelper(Context context, String name,
+      SQLiteDatabase.CursorFactory factory, int version) {
+    super(context, name, factory, version);
+  }
+
+  @Override public void onCreate(SQLiteDatabase db) {
+
+  }
+
+  @Override public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+  }
+}
