97bf5aa3f52ffc68909df0948cd0c90a627c789a
Marat Tlepberghenov <mur.marat@gmail.com>
2018-06-23 13:25:51 +0300

    Creates checkData method

--- app/src/main/java/com/tmarat/workmanger/Presenter.java
+++ app/src/main/java/com/tmarat/workmanger/Presenter.java
@@ -9,4 +9,11 @@ public class Presenter implements Contract.Presenter {
     this.view = view;
     model = new Model();
   }
+
+  private void checkData() {
+    Person person = view.getUserInput();
+    if (person.getName().equals("") || person.getSurname().equals("") || person.getAge().equals("")) {
+      view.showSnackBar(R.string.empty_values);
+    }
+  }
 }
